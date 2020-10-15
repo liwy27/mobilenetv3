eb0e2f111566b1efb48674d0a593df5724eebec8
liwy27 <2316834847@qq.com>
2020-10-15 13:45:47 +0800

    add file

--- data_processor.py
+++ data_processor.py
@@ -9,6 +9,8 @@ INPUT_TEST_DATA = './data/valid'
 OUTPUT_TRAIN_FILE = './data/train.tfrecords'
 OUTPUT_TEST_FILE = './data/valid.tfrecords'
 
+num_records = 10
+
 
 def _int64_feature(value):
     return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
@@ -18,10 +20,14 @@ def _bytes_feature(value):
     return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
 
-def get_img_data(sub_dirs, writer, INPUT_DATA, sess):
+def get_img_data(sub_dirs, INPUT_DATA, sess):
     current_label = 0
     is_root_dir = True
     print("file directory: " + INPUT_DATA)
+
+    record_name = INPUT_DATA + "-00.tfrecords"
+    writer = tf.io.TFRecordWriter(record_name)
+
     for sub_dir in sub_dirs:
         if is_root_dir:
             is_root_dir = False
@@ -37,8 +43,7 @@ def get_img_data(sub_dirs, writer, INPUT_DATA, sess):
         for file_name in file_list:
             image_raw_data = tf.gfile.FastGFile(file_name, 'rb').read()
             image = tf.io.decode_jpeg(image_raw_data, channels=3)
-            if image.dtype != tf.float32:
-                image = tf.image.convert_image_dtype(image, dtype=tf.float32)
+            image = tf.cast(image, tf.float32) / 128. - 1
             image = tf.image.resize_images(image, [224, 224])
             image_value = sess.run(image)
             image_raw = image_value.tostring()
@@ -48,10 +53,14 @@ def get_img_data(sub_dirs, writer, INPUT_DATA, sess):
                 'label': _int64_feature(current_label)
             }))
 
-            writer.write(tf_example.SerializeToString())
             index = index + 1
-            if index == 400:
-                break
+            if index % 50 == 0:
+                writer.close()
+                record_name = INPUT_DATA + "-%.2d.tfrecords" % (index // 50)
+                print(record_name)
+                writer = tf.io.TFRecordWriter(record_name)
+            writer.write(tf_example.SerializeToString())
+
             print("处理文件索引%d index%d"%(current_label,index))
         current_label += 1
 
@@ -59,15 +68,11 @@ def get_img_data(sub_dirs, writer, INPUT_DATA, sess):
 def create_image_lists(sess):
 
     sub_dirs = [x[0] for x in os.walk(INPUT_TRAIN_DATA)]
-    writer_train = tf.io.TFRecordWriter(OUTPUT_TRAIN_FILE)
-    get_img_data(sub_dirs, writer_train, INPUT_TRAIN_DATA, sess)
+    get_img_data(sub_dirs, INPUT_TRAIN_DATA, sess)
 
     sub_test_dirs = [x[0] for x in os.walk(INPUT_TEST_DATA)]
     writer_test = tf.io.TFRecordWriter(OUTPUT_TEST_FILE)
-    get_img_data(sub_test_dirs, writer_test, INPUT_TEST_DATA, sess)
-
-    writer_train.close()
-    writer_test.close()
+    get_img_data(sub_test_dirs, INPUT_TEST_DATA, sess)
 
 
 def main():
